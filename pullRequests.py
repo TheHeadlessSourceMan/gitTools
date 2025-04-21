fb609ee7d400dd83cb74caf1473ec4bfe29fd9ca
keilander <Kurt.Eilander@osii.com>
2025-04-21 13:12:12 -0500

    add ability to get pull request

--- pullRequests.py
+++ pullRequests.py
@@ -69,7 +69,7 @@ def prHyperlink(repoUrl:UrlCompatible,prNum:typing.Union[int,str])->str:
 
 
 def getPRs(
-    localRepoPath:typing.Union[str,Path,UrlCompatible],
+    localRepoPath:typing.Union[None,str,Path,UrlCompatible]=None,
     author:typing.Optional[str]=None,
     limit:int=30,
     state:str='open',
@@ -79,6 +79,7 @@ def getPRs(
     (as a Pandas dataframe)
 
     :localRepoPath: the directory where the repo you want to inspect is at
+        if None, use current directory
     :author: can be '@me' or a name - default=None
     :limit: max number of values to return - default=30
     :state: "open","closed","merged","all" - default="open"
@@ -89,7 +90,11 @@ def getPRs(
     import pandas as pd # type: ignore
     import subprocess
     import io
-    localRepoPath=asPathlibPath(localRepoPath)
+    if localRepoPath is None:
+        localRepoPath='.'
+    # TODO: this is not working
+    # localRepoPath=asPathlibPath(localRepoPath)
+    localRepoPath=Path(localRepoPath)
     cmd=['gh','pr','list']
     if author is not None:
         cmd.append('-A')
@@ -103,7 +108,8 @@ def getPRs(
     if baseBranch is not None:
         cmd.append('-B')
         cmd.append(baseBranch)
-    po=subprocess.Popen(cmd,cwd=str(localRepoPath),stdout=subprocess.STDOUT)
+    po=subprocess.Popen(cmd,cwd=str(localRepoPath),
+        stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     out,_=po.communicate()
     data=out.decode('utf8',errors='ignore').replace('\r','').split('\n')
     data.insert(0,"PR\tTitle\tFrom Branch\tState\tTimestamp")
@@ -160,3 +166,105 @@ def updatePRBranch(
     if not result.succeeded:
         raise Exception(result.err)
     printCb('DONE')
+
+
+def checkoutPR(
+    prNumber:typing.Union[str,int],
+    repo:typing.Optional[str]=None,
+    directory:typing.Union[None,Path,str]=None,
+    branchName:typing.Optional[str]=None):
+    """
+    Check out a particular pull request
+
+    :prNumber: Which pr to check out
+        can also be a url such as https://github.com/username/REPO/pull/314
+    :repo: name of the repo to check out.  If missing, derive it from
+        the checkout directory.
+    :directory: name of the directory we will be working with. Defaults to
+        the current directory. If directory does not exist will clone the
+        given repo to create it.
+    :branchName: optionally, go for a particular branch
+    """
+    if directory is None:
+        directory=Path('.')
+    elif not isinstance(directory,Path):
+        directory=asPathlibPath(str)
+    if not isinstance(prNumber,int):
+        if prNumber.startswith('http'):
+            parts=prNumber.rsplit('/pull/',1)
+            if repo is None:
+                repo=parts[0]
+            prNumber=int(parts[1])
+        else:
+            prNumber=int(prNumber)
+    if not directory.is_dir():
+        # attempt to clone
+        if repo is None or directory.exists():
+            raise FileNotFoundError(f'ERR: no repo at "{directory}"')
+        cmd=['git','clone',str(repo),str(directory)]
+        result=osrun(cmd)
+        print(result.outerr)
+    # make sure we are on master
+    cmd=['git','checkout','master']
+    result=osrun(cmd)
+    print(result.outerr)
+    # fetch the PR and create a new branch
+    cmd=['git','fetch','origin',f'pull/{prNumber}/head:{branchName}']
+    result=osrun(cmd)
+    print(result.outerr)
+    #switch to that branch to review or test the changes locally.
+    cmd=['git','checkout',branchName]
+    result=osrun(cmd)
+    print(result.outerr)
+
+
+def cmdline(args:typing.Iterable[str])->int:
+    """
+    Run the command line
+
+    :param args: command line arguments (WITHOUT the filename)
+    """
+    didSomething=False
+    printHelp=False
+    branch=None
+    repo=None
+    directory='.'
+    for arg in args:
+        if arg.startswith('-'):
+            av=arg.split('=',1)
+            av[0]=av[0].lower()
+            if av[0] in ('-h','--help'):
+                printHelp=True
+            elif av[0] in ('--checkoutpr','--checkout'):
+                checkoutPR(av[1],repo,directory,branch)
+                didSomething=True
+            elif av[0] in ('--ls','--list'):
+                for pr in getPRs(directory,limit=200):
+                    print(pr)
+            elif av[0] in ('--branch'):
+                branch=av[1]
+            elif av[0] in ('--repo','--repository'):
+                repo=av[1]
+            elif av[0] in ('--dir','--directory'):
+                directory=av[1]
+            else:
+                printHelp=True
+        else:
+            printHelp=True
+    if printHelp or not didSomething:
+        print('USAGE:')
+        print('  pullRequests [options]')
+        print('OPTIONS:')
+        print('  -h ................ ............ this help')
+        print('  --checkout=pr_num .............. check out a pull request')
+        print('  --ls ........................... list open PRs')
+        print('  --branch=branch_name ........... select a particular branch')
+        print('  --repo=repo_name ............... select a particular repository') # noqa: E501 # pylint: disable=line-too-long
+        print('  --dir=local_directory .......... select a particular directory') # noqa: E501 # pylint: disable=line-too-long
+        return 1
+    return 0
+
+
+if __name__=='__main__':
+    import sys
+    cmdline(sys.argv[1:])
